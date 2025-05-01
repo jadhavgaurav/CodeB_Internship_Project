1e1b3071f4043c7907cc2cdcf4f3243608635ca8
Gaurav Jadhav <gaurav.vjadhav01@gmail.com>
2025-05-01 09:45:04 +0530

    deployment stage 2

--- app.py
+++ app.py
@@ -50,14 +50,16 @@ pipeline = joblib.load("xgb_pipeline.pkl")  # Make sure path is correct
 
 # ========== App UI ==========
 with st.container():
+    st.markdown('<div class="main-box">', unsafe_allow_html=True)
+
     st.markdown("""
         <div class="title-style">
-        <div style="display: flex; justify-content: center; align-items: center;">
-            <span style="font-size: 40px;">🔍</span>&nbsp;
-            <span>Phishing URL Detector</span>
+            <div style="display: flex; justify-content: center; align-items: center;">
+                <span style="font-size: 40px;">🔍</span>&nbsp;
+                <span>Phishing URL Detector</span>
+            </div>
         </div>
-        </div>
-        """, unsafe_allow_html=True)
+    """, unsafe_allow_html=True)
 
 
     # st.markdown('<div class="title-style">🔍 Phishing URL Detector</div>', unsafe_allow_html=True)
